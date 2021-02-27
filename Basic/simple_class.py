6983a9c4bef731c9a87a1682940793179046686d
Kimsehwan96 <sktpghks138@gmail.com>
2021-02-27 18:42:50 +0900

    add del method in simple_class

--- Basic/simple_class.py
+++ Basic/simple_class.py
@@ -7,6 +7,10 @@ class Message:
     def __str__(self):  # 인스턴스를 그냥 호출했을 때 콘솔에서 보일 내용을 정의함.
         return f'message content : {self.content}'
 
+    def __del__(self):
+        print("this message will be deleted from memory")
+        del self
+
     def send(self, target=None) -> bool:  # 사실 이런 메서드들은 정적 메서드로 구현하는것이 더 올바른 방식임.
         if target is None:
             print('you should put where you want to send message')
@@ -21,5 +25,17 @@ class Message:
 
 if __name__ == '__main__':
     msg = Message("안녕하세요")
+    print(msg) # message content : 안녕하세요
     targets = ['kim', 'lee', 'park']
     msg.send(targets)
+
+    #참조가 끝난 이후에 알아서 __del__ 메서드가 수행됨
+    #최종 아웃풋
+
+    '''
+    message content : 안녕하세요
+    send msg successfully target : kim
+    send msg successfully target : lee
+    send msg successfully target : park
+    this message will be deleted from memory
+    '''
