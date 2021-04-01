a859da56d943af6031788b09fcf8d8d9fc76905d
Kimsehwan96 <sktpghks138@gmail.com>
2021-04-02 02:21:12 +0900

    add LinkedList Bases

---
+++ Pythonic/data_structure/LinkedList.py
@@ -0,0 +1,22 @@
+from abc import ABCMeta, abstractmethod
+from .node import SingleNode
+from typing import Any
+
+
+class LinkedList(metaclass=ABCMeta):
+    def __init__(self):
+        self._len = 0
+        self._head = SingleNode()
+        self._cursor = self._head
+
+    @abstractmethod
+    def append(self, data):
+        raise NotImplementedError
+
+    @abstractmethod
+    def pop(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def cur_value(self) -> Any:
+        raise NotImplementedError
---
+++ Pythonic/data_structure/SingleLinkedList.py
@@ -0,0 +1,20 @@
+from .LinkedList import LinkedList
+from .node import SingleNode
+
+
+class SingLinkedList(LinkedList):
+
+    def __iter__(self):
+        pass
+
+    def append(self, data):
+        pass
+
+    def pop(self):
+        pass
+
+    def cur_value(self):
+        pass
+
+
+# TODO: implement above methods !!
